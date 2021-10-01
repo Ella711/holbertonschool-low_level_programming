ab50173d19702121cd049b3b1bc921c0ad5bfd38
Ella711 <allenro711@gmail.com>
2021-10-01 15:44:28 -0500

    Added file for task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Prints numbers 0-9
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 48; i < 103; i++)
+	{
+		if(i == 58)
+		{
+			i = 97;
+		}
+		putchar(i);
+	}
+	putchar (10);
+	return (0);
+}
