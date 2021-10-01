e31f3f74bb9b08847418165774050c07090b1c05
Ella711 <allenro711@gmail.com>
2021-10-01 15:38:07 -0500

    Added file for task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Prints alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar (10);
+	return (0);
+}
