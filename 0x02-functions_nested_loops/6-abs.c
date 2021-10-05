8a82335424f8383ff408b0b580ae1d38eba162b2
Ella711 <allenro711@gmail.com>
2021-10-05 11:13:01 -0500

    Fixed betty for task6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
-* int _abs(int) - prints absolute value of int
+*_abs - prints absolute value of int
+*@x: any integer
+*Return: x
 */
 
 int _abs(int x)
 {
-	if ( x < 0 )
+	if (x < 0)
 	{
-		x = x *-1;
-	} 
+		x = x * -1;
+	}
 	return (x);
 }
+
