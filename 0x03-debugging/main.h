08f578d1399442cf80ea3e824ee8d9baf117cbc6
Ella711 <allenro711@gmail.com>
2021-10-06 12:57:21 -0500

    Fixed main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,17 +3,16 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <time.h>
 
 /*positive_or_negative - Prints positive or negative*/
 int positive_or_negative(int i);
 /*largest_number - returns the largest of 3 numbers*/
 int largest_number(int a, int b, int c);
-/*convert_day - converts day of month to day of year, without accounting
-*for leap year*/
+/*convert_day - converts day of month to day of year*/
 int convert_day(int month, int day);
-/*print_remaining_days - takes a date and prints how many days are
-*left in the year, taking leap years into account*/
+/*print_remaining_days - prints remaining days*/
 void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
