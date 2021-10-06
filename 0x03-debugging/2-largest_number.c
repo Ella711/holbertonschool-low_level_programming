1c4883b46a909755a21c2c20bb3b88b90d3ba0d4
Ella711 <allenro711@gmail.com>
2021-10-06 13:52:49 -0500

    Fixed betty style for task 2 and 3

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,7 +10,7 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
 	if (a >= b && a >= c)
 	{
@@ -31,3 +31,4 @@ int largest_number(int a, int b, int c)
 
 	return (largest);
 }
+
