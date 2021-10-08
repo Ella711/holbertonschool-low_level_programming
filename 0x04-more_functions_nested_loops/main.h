2d701b2627bacdfaa8cd36610ee06d820ec18b17
Ella711 <allenro711@gmail.com>
2021-10-08 11:52:52 -0500

    Added Readme and main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+/* Define libraries */
+#include <unistd.h>
+#include <ctype.h>
+#include <time.h>
+
+/* Prints char */
+int _putchar(char c);
+/* checks for uppercase characters*/
+int _isupper(int c);
+/* function that checks for a digit (0 through 9) */
+int _isdigit(int c);
+/* function that multiplies two integers */
+int mul(int a, int b);
+/* function that prints numbers followed by a new line */
+void print_numbers(void);
+/*  prints numbers 0 to 9 skipping 2 and 4 */
+void print_most_numbers(void);
+/* prints 10 times the numbers, from 0 to 14 */
+void more_numbers(void);
+/* draws a straight line in the terminal */
+void print_line(int n);
+/* prints a triangle, followed by a new line */
+void print_triangle(int size);
+
+#endif /* MAIN_HEADER */
