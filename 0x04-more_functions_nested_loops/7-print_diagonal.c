79052699a39f5bf05e6313ecf2f2bf88ecb5c1a9
Ella711 <allenro711@gmail.com>
2021-10-08 12:31:18 -0500

    Fixed line spacing

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,22 +8,21 @@
 
 void print_diagonal(int n)
 {
-    int y;
-    int x;          
+	int y;
+	int x;
 
-      if( n <= 0)
-      {
-         
-        _putchar(10);
-      }
-        for(y= 0; y<n; y++)
-    {
-            for(x =1; x <= y; x++)
-            {
-                _putchar(' ');
-            }
-        _putchar(92);    
-        _putchar(10); 
-    }  
- 
+	if (n <= 0)
+	{
+		_putchar(10);
+	}
+	for (y = 0; y < n; y++)
+	{
+		for (x = 1; x <= y; x++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		_putchar(10);
+	}
 }
+
