ba093d6ac83a02cf07c56bd106be507816ba0d6f
Ella711 <allenro711@gmail.com>
2021-10-11 13:35:44 -0500

    Added file for task3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -13,5 +13,7 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 /* returns length of string */
 int _strlen(char *s);
+/* prints the string */
+void _puts(char *str);
 
 #endif /* MAIN_HEADER */
