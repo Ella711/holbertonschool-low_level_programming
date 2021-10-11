d3e9f35aa246f87bb2b8e440a8796744fc8e9fa4
Ella711 <allenro711@gmail.com>
2021-10-11 11:55:30 -0500

    Added files for task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* swap_int - swaps integers
+*@*a: any integer
+*@*b" any integer
+*/
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,5 +9,7 @@
 int _putchar(char c);
 /* update pointer value */
 void reset_to_98(int *n);
+/* swaps the value of integers */
+void swap_int(int *a, int *b);
 
 #endif /* MAIN_HEADER */
