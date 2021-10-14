8edbe27256669607f6f3f8218a94f796ada6ca7d
Ella711 <allenro711@gmail.com>
2021-10-14 13:33:55 -0500

    Modify main.h for new prototype

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,5 +19,7 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 /* print array in reverse */
 void reverse_array(int *a, int n);
+/* changes all lowercase letters of a string to uppercase */
+char *string_toupper(char *);
 
 #endif /* MAIN_HEADER */
