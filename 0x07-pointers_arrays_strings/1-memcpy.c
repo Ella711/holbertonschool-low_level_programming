2fa0afe39d7acce68f8c685c4819f5e54d0849ae
Ella711 <allenro711@gmail.com>
2021-10-18 12:48:57 -0500

    [FIX] Code for task1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,16 +10,10 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *csrc = src;
-	char *cdest = dest;
+	unsigned int i;
 
-	while (n > 0)
-	{
-		*cdest = *csrc;
-		cdest++;
-		csrc++;
-		n--;
-	}
-	return (cdest);
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+	return (dest);
 }
 
