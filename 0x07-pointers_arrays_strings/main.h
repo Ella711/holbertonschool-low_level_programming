07a32524e20f448dc0e832c8402e46d1f1a638a1
Ella711 <allenro711@gmail.com>
2021-10-18 13:40:05 -0500

    [ADD] Prototype for task2

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,7 @@
 char *_memset(char *s, char b, unsigned int n);
 /* function that copies memory area */
 char *_memcpy(char *dest, char *src, unsigned int n);
+/* function that locates a character in a string */
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_HEADER */
