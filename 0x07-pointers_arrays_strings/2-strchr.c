3b61919bdc82d8aac291f47968d41e9a1684e181
Ella711 <allenro711@gmail.com>
2021-10-18 13:40:32 -0500

    [ADD] File for task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a char in a string
+ * @s: any string
+ * @c: char to be located
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (; *s; s++)
+	{
+		if (*s == c)
+		{
+			return(s);
+		}
+	}
+	if (*s != c)
+	{
+		return (s);
+	}
+	return (s);
+}
