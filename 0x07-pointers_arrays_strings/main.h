0f9580b7e8a0990843bff0655727cae778c83039
Ella711 <allenro711@gmail.com>
2021-10-18 15:36:03 -0500

    [ADD] Prototype for function for task3

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,5 +11,7 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 /* function that locates a character in a string */
 char *_strchr(char *s, char c);
+/* function that gets length of a prefix substring */
+unsigned int _strspn(char *s, char *accept);
 
 #endif /* MAIN_HEADER */
