9d39d9a8c88203814ab867fe264d2ff4cceb157d
Ella711 <allenro711@gmail.com>
2021-10-18 13:41:41 -0500

    [FIX] Betty styling

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,7 +13,7 @@ char *_strchr(char *s, char c)
 	{
 		if (*s == c)
 		{
-			return(s);
+			return (s);
 		}
 	}
 	if (*s != c)
@@ -22,3 +22,4 @@ char *_strchr(char *s, char c)
 	}
 	return (s);
 }
+
