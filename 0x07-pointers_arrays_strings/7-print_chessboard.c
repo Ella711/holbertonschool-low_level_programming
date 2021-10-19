4c2beca8916dee97476ac0d5fc7850fdafd0b446
Ella711 <allenro711@gmail.com>
2021-10-19 15:21:00 -0500

    [FIX] Betty styling for task6-7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
- /**
- * print_chessboard - prints the chessboard
- * @a: any array
- */
+/**
+* print_chessboard - prints a chessboard
+*@a: any array
+*/
 
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
-	
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
@@ -16,3 +16,4 @@ void print_chessboard(char (*a)[8])
 		_putchar(10);
 	}
 }
+
