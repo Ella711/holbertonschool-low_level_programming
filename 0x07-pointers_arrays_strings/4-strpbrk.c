ec5e7fe4b61dd1fcfb9e44e9ad6aa24d8a063336
Ella711 <allenro711@gmail.com>
2021-10-18 19:58:49 -0500

    [ADD] File for task4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: any string
+ * @accept: target string
+ * Return: s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *pa = accept;
+
+	while (*s)
+	{
+		for (accept = pa; *accept; accept++)
+		{
+			if (*s == *accept)
+				return (s);
+		}
+		s++;
+	}
+	if (*s == '\0' && *accept == '\0')
+	{
+		return (NULL);
+	}
+	return (s);
+}
+
