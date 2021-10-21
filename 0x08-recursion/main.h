0646665b4ce011e5d81ad4893f17296bc887fcb3
Ella711 <allenro711@gmail.com>
2021-10-20 23:18:50 -0500

    [UPDATE] prototypes for task1

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,9 @@
 int _putchar(char c);
 /* prints a string, followed by a new line */
 void _puts_recursion(char *s);
+/* prints a string in reverse */
+void _print_rev_recursion(char *s);
+/* finds the length of string */
+int _strlen(char *s);
 
 #endif /* MAIN_HEADER */
