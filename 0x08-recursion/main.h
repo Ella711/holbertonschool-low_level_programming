01a49509d32addb540db4a4f59c9dba4ba99e1c1
Ella711 <allenro711@gmail.com>
2021-10-20 22:40:22 -0500

    [ADD] main.h file with prototypes for task0

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+/* Define libraries */
+#include <unistd.h>
+#include <ctype.h>
+#include <time.h>
+#include <stdlib.h>
+
+/* prints a char */
+int _putchar(char c);
+/* prints a string, followed by a new line */
+void _puts_recursion(char *s);
+
+#endif /* MAIN_HEADER */
