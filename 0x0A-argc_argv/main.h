afd01b54083b0966c9f00b232f1644f3e872d227
Ella711 <allenro711@gmail.com>
2021-10-22 15:10:32 -0500

    [ADD] main.h for libraries

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+/* Define libraries */
+#include <unistd.h>
+#include <ctype.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#endif /* MAIN_HEADER */
