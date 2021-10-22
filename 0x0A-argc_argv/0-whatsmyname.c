b62dbf7e737880678ddacc00c71ece735f11dc07
Ella711 <allenro711@gmail.com>
2021-10-22 15:11:08 -0500

    [ADD] Files for task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - prints it's name
+* @argc: - arguement counter
+* @argv: - pointer string
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", *argv);
+	return (0);
+}
+
