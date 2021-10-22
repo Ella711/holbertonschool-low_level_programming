c46c2c4a0bd3a0dfad3212341b0867e1ccdb8247
Ella711 <allenro711@gmail.com>
2021-10-21 23:35:19 -0500

    [UPDATE] Prototype for task6

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -22,5 +22,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 /* returns sqr root of n */
 int _sqrt_recursion(int n);
+/* returns 1 if prime, otherwise 0 */
+int is_prime_number(int n);
 
 #endif /* MAIN_HEADER */
