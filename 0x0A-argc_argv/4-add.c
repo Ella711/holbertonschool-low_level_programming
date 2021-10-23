f962623f4261f1a36b696a3baf7e74feda5083f5
Ella711 <allenro711@gmail.com>
2021-10-22 19:42:42 -0500

    [ADD] File for task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+* main - prints it's name
+* @argc: - arguement counter
+* @argv: - pointer string
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+
+			if (isdigit(*argv[i]))
+			{
+				sum += strtol(argv[i], NULL, 10);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+			
+		}printf("%d\n", sum);
+		
+	}
+	
+	return (0);
+}
