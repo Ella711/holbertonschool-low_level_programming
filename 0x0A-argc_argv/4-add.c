127b8f911da0723e74d010d7c085d4555f52ab0e
Ella711 <allenro711@gmail.com>
2021-10-22 21:18:35 -0500

    [FIX] code to validate negative numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,31 @@
 #include "main.h"
 
 /**
+ * isnum - return 1 if string is a number else 0
+ * @string: string to be evaluated
+ * Return: success or failure
+ */
+
+int isnum(char *string)
+{
+	int j = 0;
+
+	for (j = 0; string[j]; j++)
+	{
+		if (string[j] == '-')
+		{
+			j++;
+		}
+		if (!isdigit(string[j]))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+
+/**
 * main - prints it's name
 * @argc: - arguement counter
 * @argv: - pointer string
@@ -21,7 +46,7 @@ int main(int argc, char *argv[])
 	for (i = 1; i < argc; i++)
 	{
 
-		if (isdigit(*argv[i]))
+		if (isnum(argv[i]) != 0)
 		{
 			sum += strtol(argv[i], NULL, 10);
 		}
@@ -32,7 +57,6 @@ int main(int argc, char *argv[])
 		}
 	}
 	printf("%d\n", sum);
-
 	return (0);
 }
 
