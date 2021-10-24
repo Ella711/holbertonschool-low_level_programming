f3a03c17842c7fdb91747c002cd69e9a062abc46
Ella711 <allenro711@gmail.com>
2021-10-24 13:37:17 -0500

    [FIX] strlen n

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,9 +12,26 @@ int palindrome(char *str, int i, int j)
 {
 	if (i > j)
 		return (1);
-	if (str[i] != str[j])
+	else if (str[i] != str[j])
 		return (0);
-	return (palindrome(str, i + 1, j + 1));
+	return (palindrome(str, i + 1, j - 1));
+}
+
+/**
+* _strlen_recursion - returns length of string
+*@s: a string
+*Return: n
+*/
+
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (*s != '\0')
+	{
+		n = 1 + _strlen_recursion(s + 1);
+	}
+	return (n);
 }
 
 /**
@@ -25,6 +42,5 @@ int palindrome(char *str, int i, int j)
 
 int is_palindrome(char *s)
 {
-	return (palindrome(s, 0, _strlen((s) - 1)));
+	return (palindrome(s, 0, (_strlen_recursion(s) - 1)));
 }
-
