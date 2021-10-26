0e0d68235392854130336c478d065af0630ecd38
Ella711 <allenro711@gmail.com>
2021-10-26 13:22:06 -0500

    [ADD] file for task0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars,
+ * initialized with a specific char
+ * @c: char to print
+ * @size: the size of the memory to print
+ * Return: Nothing.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	array = (char *)malloc(sizeof(char) * 98);
+	if (array == NULL)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+
+}
+
