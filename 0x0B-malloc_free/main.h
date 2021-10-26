7b760f33828f7c6a9a93d636e54371723a7da317
Ella711 <allenro711@gmail.com>
2021-10-26 13:18:24 -0500

    [ADD] main.h header for new directory

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+/* Define libraries */
+#include <unistd.h>
+#include <ctype.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* creates an array of chars, and initializes it with a 
+specific char */
+char *create_array(unsigned int size, char c);
+
+#endif /* MAIN_HEADER */
