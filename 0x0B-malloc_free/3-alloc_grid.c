33c958b1763d854b89cf2f7447e8593ac6a3cf7b
Ella711 <allenro711@gmail.com>
2021-10-26 18:05:17 -0500

    [ADD] File for task3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*alloc_grid - returns length of string
+*@width: columns of 2d array
+*@height: rows of 2d array
+*Return: p
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int i;
+
+	p = (int **)malloc(width * sizeof(int *)); /* columns */
+	for (i = 0; i < width; i++)
+	{
+		p[i] = (int *)malloc(height * sizeof(int)); /* rows */
+	}
+	if (p == NULL)
+		return (NULL);
+	return (p);
+}
+
