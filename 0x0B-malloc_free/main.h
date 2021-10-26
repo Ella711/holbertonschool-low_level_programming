8a8bf8e1a7603d89724478c617fa45dd39125d2f
Ella711 <allenro711@gmail.com>
2021-10-26 14:10:30 -0500

    [UPDATE] with prototype for task1

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -10,5 +10,8 @@
 /* creates an array of chars, and initializes it with a 
 specific char */
 char *create_array(unsigned int size, char c);
+/* returns a pointer to a newly allocated space in 
+memory, which contains a copy of the string */
+char *_strdup(char *str);
 
 #endif /* MAIN_HEADER */
