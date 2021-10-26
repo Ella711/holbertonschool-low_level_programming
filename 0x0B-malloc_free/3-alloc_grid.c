792fe9a740f48140188e5ee2221dca2ec7c2b25e
Ella711 <allenro711@gmail.com>
2021-10-26 18:28:01 -0500

    [FIX] memory allocation size

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,10 +12,13 @@ int **alloc_grid(int width, int height)
 	int **p;
 	int i;
 
+	if (width == 0 && height == 0)
+		return (NULL);
+
 	p = (int **)malloc(width * sizeof(int)); /* columns */
 	for (i = 0; i < width; i++)
 	{
-		p[i] = (int *)malloc(height * sizeof(int)); /* rows */
+		p[i] = (int *)malloc(height * sizeof(int *)); /* rows */
 	}
 	if (p == NULL)
 		return (NULL);
