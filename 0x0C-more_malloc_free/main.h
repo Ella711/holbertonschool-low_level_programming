1590a3fe6f5f10613138705166925ce1a961db9b
Ella711 <allenro711@gmail.com>
2021-10-28 16:52:34 -0500

    [ADD] header file

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+/* Define libraries */
+#include <unistd.h>
+#include <ctype.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+/* allocates memory using malloc */
+void *malloc_checked(unsigned int b);
+
+
+#endif /* MAIN_HEADER */
