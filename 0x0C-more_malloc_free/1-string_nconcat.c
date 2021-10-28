cea13acbedcd7da59ab1d0c30d8f45b7da7f0bcf
Ella711 <allenro711@gmail.com>
2021-10-28 17:50:46 -0500

    [ADD] file for task1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+* _strlen - returns length of string
+*@s: a string
+*Return: i
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+
+/**
+ * string_nconcat - concatenates two strings limited by n
+ * @s1: string
+ * @s2: string 2
+ * @n: integer of bytes to be copied
+ * Return: malloc string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, ss1, ss2, size;
+	char *concat;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	ss1 = _strlen(s1);
+	ss2 = _strlen(s2);
+	size = ss1 + n;
+
+	if (n > ss2)
+		size = ss1 + ss2;
+
+	concat = malloc(sizeof(char) * (size + 1));
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)
+		concat[i] = s1[i];
+	for (j = 0; j < size; j++, ss1++)
+	{
+		concat[i + j] = s2[j];
+		if (j == n)
+			break;
+	}
+	return (concat);
+}
+
