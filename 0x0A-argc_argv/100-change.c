c4f4d98cb2c71363e7477966a6096e9aa2893280
Ella711 <allenro711@gmail.com>
2021-10-27 21:30:49 -0500

    [FIX] conditional for negative numbers

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -18,12 +18,15 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	else
+	
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
 	{
-		cents = atoi(argv[1]);
-		if (cents < 0)
-			printf("0");
+		printf("0\n");
+		return (0);
 	}
+
 	for (; n[i]; i++)
 	{
 		if (cents >= n[i])
