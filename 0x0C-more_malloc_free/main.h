5277c470c92359539ac9a5e9a8528b7257046144
Ella711 <allenro711@gmail.com>
2021-10-28 21:29:03 -0500

    [UPDATE] prototype for task3

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -14,6 +14,8 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 /* allocates memory for an array using malloc */
 void *_calloc(unsigned int nmemb, unsigned int size);
+/* created an array of integers */
+int *array_range(int min, int max);
 
 
 #endif /* MAIN_HEADER */
