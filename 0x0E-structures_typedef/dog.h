a0015d94a9148e397d307a8382294891ab38654a
Ella711 <allenro711@gmail.com>
2021-11-01 18:22:55 -0500

    [FIX] Commentary for prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,6 @@
 #define _DOG_
 
 /**
- * dog_struct - typedef of struct dog
- */
-
-typedef struct dog dog_struct;
-
-/**
  * struct dog - structure for dogs
  * @name: first member
  * @age: second member
@@ -21,6 +15,7 @@ struct dog
 	char *owner;
 };
 
+/* function that initializes structure */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* #ifndef _DOG_ */
