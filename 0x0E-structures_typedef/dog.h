445ba3d29f382a425158a9fed5912a4333e8d8bf
Ella711 <allenro711@gmail.com>
2021-11-01 17:30:09 -0500

    [ADD] File for task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - structure for dogs
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* #ifndef _DOG_ */
