40da4eea27301f2c590dc906a0e83c6eb965e635
Ella711 <allenro711@gmail.com>
2021-11-01 18:18:32 -0500

    [FIX] Betty styling and null conditional

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdlib.h>
 
 /**
  * init_dog - function to initialize struct variable
@@ -12,6 +11,9 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
