9e45fc98208c584fff7ff70b4f01706b8df13403
Ella711 <allenro711@gmail.com>
2021-11-01 19:37:54 -0500

    [FIX] nil conditionals

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,20 +13,14 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-	{
-		d->name = NULL;
-		printf("Name: %p\n", d->name);
-	}
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 
 	printf("Age: %f\n", d->age);
-	
+
 	if (d->owner == NULL)
-	{
-		d->owner = NULL;
-		printf("Name: %p\n", d->owner);
-	}
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
