f8784e1210a8f4c1598e3486b25f028d57ef41af
Ella711 <allenro711@gmail.com>
2021-11-01 20:35:09 -0500

    [FIX] Betty styling and libraries for malloc

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
  * new_dog - createds a new dog
- * @new_dog: pointer to stored data for struct dog
- * Return: d pointer
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: owner of new dog
+ * Return: new pointer
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *pupper;
-	
+
 	pupper = malloc(sizeof(dog_t));
 	if (pupper == NULL)
 		return (NULL);
-	
+
 	pupper->name = name;
 	pupper->age = age;
 	pupper->owner = owner;
 	return (pupper);
 }
+
