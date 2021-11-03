ed0320aaf88c56e354714f3e935ded0291cc4f16
Ella711 <allenro711@gmail.com>
2021-11-03 15:31:55 -0500

    [UPDATE] Prototype for task1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -10,6 +10,8 @@
 
 /* function that prints a name */
 void print_name(char *name, void (*f)(char *));
+/* executes a function given as a parameter on each element of an array */
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 
 
