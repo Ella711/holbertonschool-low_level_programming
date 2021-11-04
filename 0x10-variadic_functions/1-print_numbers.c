cb420fdab3dcc95621f7eaefc88a94dd1540e28f
Ella711 <allenro711@gmail.com>
2021-11-04 16:53:31 -0500

    [ADD] File for task1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+* print_numbers - prints all numbers
+*@separator: string
+*@n: number of parameters
+*Return: sum of all parameters
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int i;
+
+	va_start(nums, n);
+
+	if (separator == NULL)
+		return ;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+		{
+			printf("%d\n", va_arg(nums, int));
+			break;
+		}
+		printf("%d%s", va_arg(nums, int), separator);
+	}
+	va_end(nums);
+}
