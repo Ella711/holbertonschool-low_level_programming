1c79b335d45cd3106ccfec3a6d66cf3f897d2bfb
Ella711 <allenro711@gmail.com>
2021-11-04 15:14:28 -0500

    [ADD] header file for new directory

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,14 @@
+#ifndef VARIADIC_HEADER
+#define VARIADIC_HEADER
+/* Define libraries */
+#include <unistd.h>
+#include <ctype.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/* returns the sum of all it's parameters */
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* MAIN_HEADER */
+
