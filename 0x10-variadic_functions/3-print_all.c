582452892d022726f95cd71f686ceb17a8796cb4
Ella711 <allenro711@gmail.com>
2021-11-04 22:54:43 -0500

    [FIX] Printing spaces

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,7 +6,7 @@
 *@format: format of parameters
 */
 
-void print_all(const char * const format, ...)
+void print_all(const char *const format, ...)
 {
 	va_list args;
 	char *sval;
@@ -18,31 +18,31 @@ void print_all(const char * const format, ...)
 	{
 		switch (*str++)
 		{
-			case 'c':
-				putchar(va_arg(args, int));
+		case 'c':
+			putchar(va_arg(args, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double));
+			break;
+		case 's':
+			sval = va_arg(args, char *);
+			if (sval == NULL)
+			{
+				printf("(nil)");
 				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				sval = va_arg(args, char *);
-				if (sval == NULL)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", sval);
-				break;
-			default:
-				continue;
+			}
+			printf("%s", sval);
+			break;
+		default:
+			continue;
 		}
-		if (*str)
+		if (*str++ != '\0')
 			printf(", ");
+		*str--;
 	}
 	printf("\n");
 	va_end(args);
 }
-
