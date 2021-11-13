9092f39de238565bf6552a0b8f797bc85ebfdf4a
Ella711 <allenro711@gmail.com>
2021-11-13 15:11:11 -0500

    [update] Prototype for task3

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -28,6 +28,8 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 /* adds a new node at the beginning of a list_t list */
 list_t *add_node(list_t **head, const char *str);
+/* adds a new node at the end of a list_t list */
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif /* LINKED_LISTS_HEADER */
 
