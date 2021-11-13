2a6af580f0aea9d55bcbbb6602616cf7fd63ec33
Ella711 <allenro711@gmail.com>
2021-11-13 15:37:36 -0500

    [UPDATE] Prtotype for task4

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -30,6 +30,8 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 /* adds a new node at the end of a list_t list */
 list_t *add_node_end(list_t **head, const char *str);
+/* function that frees a list_t list */
+void free_list(list_t *head);
 
 #endif /* LINKED_LISTS_HEADER */
 
