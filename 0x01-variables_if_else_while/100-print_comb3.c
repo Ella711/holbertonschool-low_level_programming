a82b42e784dbceeede1bf3ed362d1fde2a546b96
Ella711 <allenro711@gmail.com>
2021-11-12 22:20:45 -0500

    [ADD] Files for task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Prints all possible different combinations of two digits
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			if (j != i)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+			}
+			if (!(i == 8 && j == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
