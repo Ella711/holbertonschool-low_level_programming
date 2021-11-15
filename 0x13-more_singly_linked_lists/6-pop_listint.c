3af28396626b17a442a0867a764973afe5ef49d0
Ella711 <allenro711@gmail.com>
2021-11-15 14:30:20 -0500

    [ADD] File for task6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node
+ * @head: pointer to list
+ * Return: head node’s data (n)
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *traverse;
+	int num;
+
+	if (*head == NULL)
+		return (0);
+	else
+	{
+		traverse = *head;
+
+		num = traverse->n;
+
+		*head = traverse->next;
+
+		free(traverse);
+		traverse = NULL;
+		
+		return (num);
+	}
+}
