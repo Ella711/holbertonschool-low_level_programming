4cbde995f99a7c8d0e0623982450c35161262b2a
Ella711 <allenro711@gmail.com>
2021-11-16 12:45:36 -0500

    [FIX] Betty Styling

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -33,7 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	if (traverse == NULL || traverse->next == NULL)
 		return (-1);
-	
+
 	node = traverse->next->next;
 	free(traverse->next);
 	traverse->next = node;
