a7eaae8455484d9d60d1997bacbe374d21f5a5f4
Ella711 <allenro711@gmail.com>
2021-11-18 17:16:06 -0500

    [ADD] Header file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+/* Define libraries */
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* function that converts a binary number to an unsigned int */
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_HEADER */
+
