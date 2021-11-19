1f347ca64699a9befe024543881b82dd89a56f0e
Ella711 <allenro711@gmail.com>
2021-11-18 19:22:11 -0500

    [UPDATE] Protytpe for task3

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,6 +11,8 @@ unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 /* returns the value of a bit at a given index */
 int get_bit(unsigned long int n, unsigned int index);
+/* sets the value of a bit to 1 at a given index */
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif /* MAIN_HEADER */
 
