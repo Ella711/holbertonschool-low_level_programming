4ab1690b2b8784f224e79ddc2cc6d853a7402946
Ella711 <allenro711@gmail.com>
2021-11-18 20:18:24 -0500

    [FIX] For conditional

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i, count = 0;
 
-	for (i = n ^ m; i != 0; i = i >> 1)
+	for (i = n ^ m; i > 0; i = i >> 1)
 		count += i & 1;
 
 	return (count);
