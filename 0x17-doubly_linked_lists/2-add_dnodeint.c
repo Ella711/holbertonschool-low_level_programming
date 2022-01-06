97219943b5ba26c020915e0069078893b7e58689
Ella711 <allenro711@gmail.com>
2022-01-06 13:33:09 -0500

    [FIX] Spacing

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->prev = NULL;
 	new_node->n = n;
 	new_node->next = NULL;
@@ -28,7 +28,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		(*head)->prev = new_node;
 		*head = new_node;
 	}
-	
+
 	return (*head);
 }
 
