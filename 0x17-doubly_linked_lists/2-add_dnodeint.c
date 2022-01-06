4e25d1f756fc56384636c0b75586e37fc3187b9b
Ella711 <allenro711@gmail.com>
2022-01-06 13:28:50 -0500

    [ADD] File for task2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,15 +15,20 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (new_node == NULL)
 		return (NULL);
-
+	
 	new_node->prev = NULL;
 	new_node->n = n;
 	new_node->next = NULL;
 
 	new_node->next = *head;
-	*head = new_node;
-	(*head)->prev = new_node;
-
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		(*head)->prev = new_node;
+		*head = new_node;
+	}
+	
 	return (*head);
 }
 
