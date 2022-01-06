1852596db83d2d6c81554376b0b2581bf1146c7f
Ella711 <allenro711@gmail.com>
2022-01-06 15:57:47 -0500

    [ADD] File for task5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - adds node at end of list
+ * @head: pointer to head of list
+ * @index: position of node wanted
+ * Return: pointer to indexed node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	if (head == NULL)
+		return (NULL);
+
+	while (index != 0)
+	{
+		head = head->next;
+		index--;
+	}
+	return (head);
+}
+
