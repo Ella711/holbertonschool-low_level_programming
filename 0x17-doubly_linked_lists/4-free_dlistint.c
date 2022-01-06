ba83dc53f0b203bcfd0bf95c9fcca3b27656098f
Ella711 <allenro711@gmail.com>
2022-01-06 15:34:25 -0500

    [ADD] File for task4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees list
+ * @head: pointer to head of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *traverse;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		traverse = head;
+		head = head->next;
+		free(traverse);
+	}
+}
+
