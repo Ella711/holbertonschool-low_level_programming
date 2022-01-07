1a81bf5563706031458e1794a052f840a68cf603
Ella711 <allenro711@gmail.com>
2022-01-07 11:55:18 -0500

    [ADD] File for task7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,41 +9,33 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current = *h, *new_node = NULL, *ahead = NULL;
+	dlistint_t *current = *h, *new_node = NULL;
+	unsigned int i = 0;
 
-	if (h)
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	if (idx == 0)
 	{
-		new_node = malloc(sizeof(dlistint_t));
-		if (new_node == NULL)
-			return (NULL);
-		new_node->n = n;
-		new_node->next = NULL;
-		new_node->prev = NULL;
-		if (idx == 0)
-			return (add_dnodeint(h, n));
-		if (idx != 0 && current == NULL)
-			return (NULL);
-		while ((idx != 1) && (current->next != NULL))
-		{
-			current = current->next;
-			idx--;
-		}
-		if (current->next == NULL)
-		{
-			if (idx > 1)
-				return (NULL);
-			return (add_dnodeint_end(h, n));
-		}
-		else
-		{
-			ahead = current->next;
-			current->next = new_node;
-			ahead->prev = new_node;
-			new_node->next = ahead;
-			new_node->prev = current;
-			return (new_node);
-		}
+		free(new_node);
+		return (add_dnodeint(h, n));
 	}
-	return (NULL);
+	while (i < idx - 1)
+	{
+		current = current->next;
+		if (current == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		i++;
+	}
+	new_node->n = n;
+	new_node->next = current->next;
+	current->next = new_node;
+	new_node->prev = current;
+	if (new_node->next != NULL)
+		new_node->next->prev = new_node;
+	return (new_node);
 }
 
