1888bc412a5e2b1d502627c71cd648ab799736bc
Ella711 <allenro711@gmail.com>
2022-01-28 12:57:54 -0500

    Shorter code

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,15 +8,8 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash;
-	int c;
-
-	hash = 5381;
-	while ((c = *key++))
-	{
-		hash = ((hash << 5) + hash) + c;
-	}
-
-	hash = hash % size;
+	
+	hash = hash_djb2(key) % size;
 
 	return (hash);
 }
