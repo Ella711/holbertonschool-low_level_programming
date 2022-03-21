785939fc0b0a129d4b0ef598647f28c14eb9747f
Ella711 <allenro711@gmail.com>
2022-03-21 16:29:59 -0500

    Add file for task5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,18 +9,16 @@ def island_perimeter(grid):
     Returns the perimeter of the island described in grid
     """
     row, column = len(grid), len(grid[0])
-    if row > 100 or column > 100:
-        return
-    land, doubles = 0, 0
+    perimeter = 0
     for i in range(row):
         for j in range(column):
             # Find if there is a 1 on the grid and count as land
             if grid[i][j] == 1:
-                land += 1
+                perimeter += 4
                 # IF position exists AND there is a 1 there
                 # Adds to double since loop will be going through that position
-                if i < row - 1 and grid[i + 1][j] == 1:
-                    doubles += 1
-                if j < column - 1 and grid[i][j - 1] == 1:
-                    doubles += 1
-    return land * 4 - 2 * doubles
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
